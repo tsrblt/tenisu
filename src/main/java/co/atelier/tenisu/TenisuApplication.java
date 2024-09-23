3a80be54beb25e659e1475216905630576777565
Bagrind <t_raimbault@yahoo.fr>
2024-09-23 15:09:48 +0200

    Initial commit

---
+++ src/main/java/co/atelier/tenisu/TenisuApplication.java
@@ -0,0 +1,13 @@
+package co.atelier.tenisu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenisuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TenisuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/atelier/tenisu/TenisuApplicationTests.java
@@ -0,0 +1,13 @@
+package co.atelier.tenisu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenisuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
